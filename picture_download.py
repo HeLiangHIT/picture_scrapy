68cec4ae545824cbda54b134a01c361711ef8025
heliang <1042248300@qq.com>
2018-10-14 11:58:56 +0800

    add picture downloader

--- picture_download.py
+++ picture_download.py
@@ -4,15 +4,13 @@
 # @Author  : He Liang (heianghit@foxmail.com)
 # @Link    : https://github.com/HeLiangHIT
 
+from faker import Faker
+import redis, json, os, logging, trio, asks, random
+asks.init('trio')
 
-from downloader_async import FileDownloader, ProgressBar
-import redis, json, os, logging, trio
 
-
-_SAVE_DIR = "%s/Pictures/scrapy/" % os.path.expanduser('~') # os.environ['HOME']
-logging.basicConfig(level=logging.DEBUG, 
-    format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
-
+logging.basicConfig(level=logging.INFO, 
+                format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
 
 _db = redis.StrictRedis(host='127.0.0.1', port=6379, password=None, decode_responses=True)
 def get_picture_item(key='picture:mmjpg'):
@@ -20,28 +18,48 @@ def get_picture_item(key='picture:mmjpg'):
     data = _db.spop(key) # await ?
     return json.loads(data) if data is not None else None
 
+def is_picture_exists(key='picture:mmjpg'):
+    return _db.scard(key)
 
-_downloader = FileDownloader()
-async def download_item(item):
-    header = {"Host": item['url'].split('/')[3]}
-    isok, file_name, file_size = await _downloader.download(item['url'], 
-        _SAVE_DIR + item['folder'],
-        progress_callback = ProgressBar("download %s..." % item['name']), 
-        headers = header)
-    return isok
+def save_failed_item(key='picture:mmjpg', item=None):
+    if isinstance(item, dict):
+        _db.sadd("%s:failed" % key, json.dumps(item))
+        logging.info(f"{item} was saved in redis]key={key}:failed.")
+
+
+async def download_picture(url, res_time=10):
+    if res_time <= 0: # 重试超过了次数
+        return None
+    header = {"Host": url.split('/')[3], "Referer": url, "User-Agent":Faker(locale='zh').chrome()}
+    res = await asks.get(url) # , headers=dict(**header, **_HEADERS), timeout=60, retries=5
+    if res.status_code not in [200, 202]:
+        await trio.sleep(random.randint(3, 10))
+        return await download_picture(url, res_time-1)
+    return res.content
+
+
+_SAVE_DIR = "%s/Pictures/scrapy/" % os.path.expanduser('~') # os.environ['HOME']
+async def save_item(folder, name, content):
+    _folder = os.path.join(_SAVE_DIR, folder)
+    os.system("mkdir -p '%s'" % _folder)
+    file_path = os.path.join(_folder, name)
+    fd = await trio.open_file(file_path, 'wb')
+    await fd.write(content)
+    await fd.aclose()
 
 
 async def main():
-    while True:
-        item = get_picture_item()
-        logging.debug(f"downloading {item['name']} from {item['url']} to {item['folder']}...")
-        if item is None:
-            break
-        isok = await download_item(item)
-        if isok:
-            logging.info(f"{item['name']} downloaded from {item['url']} to {item['folder']}")
-        else:
-            logging.error(f"download {item['name']} from {item['url']} FAIL!")
+    async with trio.CapacityLimiter(10):
+        while is_picture_exists():
+            item = get_picture_item()
+            logging.info(f"downloading {item['name']} from {item['url']} to {item['folder']}...")
+            content = await download_picture(item['url'])
+            if content is not None:
+                await save_item(item['folder'], item['name'], content)
+                logging.info(f"download {item['name']} from {item['url']} to {item['folder']} succ.")
+            else:
+                save_failed_item(item)
+                logging.error(f"download {item['name']} from {item['url']} to {item['folder']} FAIL!")
 
     logging.info("picture download over")
 
